de73a2a043e1e9b5aba92263513e47057da6081d
summingyu <summingbest@gmail.com>
2025-03-31 17:17:38 +0800

    基于Gin实现HTTP服务器
    Signed-off-by: summingyu <summingbest@gmail.com>

--- cmd/fg-apiserver/app/options/options.go
+++ cmd/fg-apiserver/app/options/options.go
@@ -1,17 +1,23 @@
 package options
 
 import (
+	"fmt"
+	"net"
+	"strconv"
+
 	"github.com/onexstack/fastgo/internal/apiserver"
 	genericoptions "github.com/onexstack/fastgo/pkg/options"
 )
 
 type ServerOptions struct {
 	MySQLOptions *genericoptions.MySQLOptions `json:"mysql" mapstructure:"mysql"`
+	Addr         string                       `json:"addr" mapstructure:"addr"`
 }
 
 func NewServerOptions() *ServerOptions {
 	return &ServerOptions{
 		MySQLOptions: genericoptions.NewMySQLOptions(),
+		Addr:         "0.0.0.0:6666",
 	}
 }
 
@@ -19,11 +25,24 @@ func (o *ServerOptions) Validate() error {
 	if err := o.MySQLOptions.Validate(); err != nil {
 		return err
 	}
+	if o.Addr == "" {
+		return fmt.Errorf("server address cannot be empty")
+	}
+	_, portStr, err := net.SplitHostPort(o.Addr)
+	if err != nil {
+		return fmt.Errorf("invalid server address format '%s': %w", o.Addr, err)
+	}
+	port, err := strconv.Atoi(portStr)
+
+	if err != nil || port < 1 || port > 65535 {
+		return fmt.Errorf("invalid server port '%s'", portStr)
+	}
 	return nil
 }
 
 func (o *ServerOptions) Config() (*apiserver.Config, error) {
 	return &apiserver.Config{
 		MySQLOptions: o.MySQLOptions,
+		Addr:         o.Addr,
 	}, nil
 }
--- internal/apiserver/server.go
+++ internal/apiserver/server.go
@@ -1,24 +1,45 @@
 package apiserver
 
 import (
+	"errors"
 	"log/slog"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	genericoptions "github.com/onexstack/fastgo/pkg/options"
 )
 
 type Config struct {
 	MySQLOptions *genericoptions.MySQLOptions
+	Addr         string
 }
 
 type Server struct {
 	cfg *Config
+	srv *http.Server
 }
 
 func (cfg *Config) NewServer() (*Server, error) {
-	return &Server{cfg: cfg}, nil
+	engine := gin.New()
+	// 注册404 Handler
+	engine.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{"code": "PageNotFound", "message": "Page not found."})
+	})
+	// 注册/healthz handler.
+	engine.GET("/healthz", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+
+	httpsrv := &http.Server{Addr: cfg.Addr, Handler: engine}
+	return &Server{cfg: cfg, srv: httpsrv}, nil
 }
 
 func (s *Server) Run() error {
 	slog.Info("Read MySQL host from config", "mysql.addr", s.cfg.MySQLOptions.Addr)
-	select {}
+	slog.Info("Start to listening to incoming requests on http address", "addr", s.cfg.Addr)
+	if err := s.srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+	defer s.srv.Close()
+	return nil
 }
